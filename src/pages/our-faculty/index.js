1b6f1a07c1917a225cb47e51fee50a17f72e6bba
BEarle2662 <bearle@gitam.edu>
2024-10-07 17:14:05 +0530

    slugPages

--- src/pages/our-faculty/index.js
+++ src/pages/our-faculty/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import MainLayout from "@/components/MainContainer/MainLayout";
 import CategoryHeading from "@/components/common/categoryHeading";
