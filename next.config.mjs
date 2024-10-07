e333bdf6c013c8ea371862ffdf5433da0ca06650
BEarle2662 <bearle@gitam.edu>
2024-10-07 17:24:45 +0530

    nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -7,15 +7,15 @@ const nextConfig = {
     unoptimized: true,
   },
   trailingSlash: true,
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.alias["react"] = path.resolve("./node_modules/react");
-      config.resolve.alias["react-dom"] = path.resolve(
-        "./node_modules/react-dom"
-      );
-    }
-    return config;
-  },
+  // webpack: (config, { isServer }) => {
+  //   if (!isServer) {
+  //     config.resolve.alias["react"] = path.resolve("./node_modules/react");
+  //     config.resolve.alias["react-dom"] = path.resolve(
+  //       "./node_modules/react-dom"
+  //     );
+  //   }
+  //   return config;
+  // },
   // output: "export",
   // assetPrefix: "https://webdev.gitam.edu/kautilya-next/",
   // assetPrefix: "https://guprojects.gitam.edu/ksppnext",
